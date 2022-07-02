c5c6616b2debe5b35f0736b4a1c691eac06f5b57
Rajeshkanna <100297459+rajeshkannapurushothaman@users.noreply.github.com>
2022-07-02 13:48:16 +0530

    fileopen and fileupdate
    modification in file

---
+++ open/fileopen.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  2 13:30:09 2022
+
+@author: rajeshkanna
+"""
+
+import os, re
+
+# filereading
+with open(r'D:\GitHub\Personal\Python-Programs\open\fire.csv', 'r') as txtfile:
+    filecont = txtfile.read()
+    txtfile.close()
+
+#content modification
+result = re.sub(r"^\"\"\,([0-9]+\,)", "\\1", filecont, 0, re.MULTILINE)
+
+
+# filesaving
+file = open(r'D:\GitHub\Personal\Python-Programs\open\output.csv','w') 
+file.write(result)
+file.close() 
